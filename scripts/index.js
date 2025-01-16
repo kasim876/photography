1e96814d677fe3b2c5e72d1df995efa7eb234770
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2025-01-17 00:13:33 +0300

    add modal open function

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("nav");
+const btn = document.getElementById("btn");
+const modal = document.getElementById("modal");
 
 const menuActiveClass = "nav--active";
 const burgerActiveClass = "burger--active";
+const modalActiveClass = "modal-wrapper--active";
 
 function openMobileMenu() {
     menu.classList.toggle(menuActiveClass);
     burger.classList.toggle(burgerActiveClass);
 }
 
+function openModal() {
+    modal.classList.add(modalActiveClass);
+}
+
+function closeModal(event) {
+    if (event.target === modal) {
+        modal.classList.remove(modalActiveClass);
+    }
+}
+
 burger.addEventListener("click", openMobileMenu);
+btn.addEventListener("click", openModal);
+modal.addEventListener("click", closeModal)
