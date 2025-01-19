390aa2c1109b63ec38fdd7ab50a7271f7c745233
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2025-01-19 08:08:09 +0300

    add disable scroll when menu or modal is open

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,12 +1,16 @@
-const burger = document.getElementById("burger");
-const menu = document.getElementById("nav");
+(function() {
+    const burger = document.getElementById("burger");
+    const menu = document.getElementById("nav");
 
-const menuActiveClass = "nav--active";
-const burgerActiveClass = "burger--active";
+    const menuActiveClass = "nav--active";
+    const burgerActiveClass = "burger--active";
+    const lockScrollClass = "stop-scroll";
 
-function toggleNavMenu() {
-    burger.classList.toggle(burgerActiveClass);
-    menu.classList.toggle(menuActiveClass);
-}
+    function toggleNavMenu() {
+        burger.classList.toggle(burgerActiveClass);
+        menu.classList.toggle(menuActiveClass);
+        document.body.classList.toggle(lockScrollClass);
+    }
 
-burger.addEventListener("click", toggleNavMenu);
+    burger.addEventListener("click", toggleNavMenu);
+})()
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,24 +1,31 @@
-const btn = document.getElementById("btn");
-const modal = document.getElementById("modal");
+(function() {
+    const btn = document.getElementById("btn");
+    const modal = document.getElementById("modal");
 
-const modalActiveClass = "modal-wrapper--active";
+    const modalActiveClass = "modal-wrapper--active";
+    const lockScrollClass = "stop-scroll";
 
-function openModal() {
-    modal.classList.add(modalActiveClass);
-}
-
-function closeModal(event) {
-    modal.classList.remove(modalActiveClass);
-}
-
-btn.addEventListener("click", openModal);
-modal.addEventListener("click", function(event) {
-    if (event.target == modal) {
-        closeModal();
+    function openModal() {
+        modal.classList.add(modalActiveClass);
+        document.body.classList.toggle(lockScrollClass);
+        console.log(1);
+        
     }
-})
-window.addEventListener("keydown", function(event) {
-    if (event.key == 'Escape') {
-        closeModal();
+
+    function closeModal() {
+        modal.classList.remove(modalActiveClass);
+        document.body.classList.toggle(lockScrollClass);
     }
-})
+
+    btn.addEventListener("click", openModal);
+    modal.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            closeModal();
+        }
+    })
+    window.addEventListener("keydown", function(event) {
+        if (event.key == 'Escape') {
+            closeModal();
+        }
+    })
+})();
